d4f11e84e96db059576570a2a7b03fa6f0896352
Crunc2h <frksythngl@hotmail.com>
2024-05-22 01:50:18 +0300

    Amended link extraction regex + Added new node creation from links and images embedded in text nodes upon extraction

--- src/inline_text_node.py
+++ src/inline_text_node.py
@@ -6,7 +6,7 @@ from text_types import TextType
 class InlineTextNode:
 
     REGX_IMAGE = r"!\[(.*?)\]\((.*?)\)"
-    REGX_LINK = r"\[(.*?)\]\((.*?)\)"
+    REGX_LINK = r"(?<!!)\[(.*?)\]\((.*?)\)"
 
     
     def split_nodes_delimiter(old_nodes, delimiter, target_text_type):
@@ -29,6 +29,78 @@ class InlineTextNode:
                             final_nodes.append(TextNode(text=text_pieces[i],
                                                         text_type=target_text_type))
         return final_nodes
+    
+    def split_nodes_image(old_nodes):
+        new_nodes = []
+        for node in old_nodes:
+            
+            if node.text_type != TextType.RAW_TEXT:
+                new_nodes.append(node)
+                continue
+            
+            original_text = node.text
+            image_contents = InlineTextNode.extract_markdown_images(original_text)
+            
+            if len(image_contents) == 0:
+                new_nodes.append(node)
+                continue
+            else:
+                for image_content in image_contents:
+                    content_string = f"[{image_content[0]}]({image_content[1]})"
+                    sections = original_text.split(content_string, 1)
+                    
+                    if len(sections) != 2:
+                        raise ValueError("Invalid markdown, image section not closed")
+                    if sections[0] != "":
+                        new_nodes.append(TextNode(text=sections[0],
+                                                  text_type=TextType.RAW_TEXT))
+                    new_nodes.append(TextNode(text=image_content[0],
+                                              text_type=TextType.IMAGE,
+                                              url=image_content[1]))
+                    original_text = sections[1]
+                if original_text != "":
+                    new_nodes.append(TextNode(text=original_text,
+                                              text_type=TextType.RAW_TEXT))
+        return new_nodes
+    
+    def split_nodes_link(old_nodes):
+        new_nodes = []
+        for node in old_nodes:
+            
+            if node.text_type != TextType.RAW_TEXT:
+                new_nodes.append(node)
+                continue
+            
+            original_text = node.text
+            link_contents = InlineTextNode.extract_markdown_links(original_text)
+            
+            if len(link_contents) == 0:
+                new_nodes.append(node)
+                continue
+            else:
+                for link_content in link_contents:
+                    content_string = f"[{link_content[0]}]({link_content[1]})"
+                    sections = original_text.split(content_string, 1)
+                    
+                    if len(sections) != 2:
+                        raise ValueError("Invalid markdown, link section not closed")
+                    if sections[0] != "":
+                        new_nodes.append(TextNode(text=sections[0],
+                                                  text_type=TextType.RAW_TEXT))
+                    new_nodes.append(TextNode(text=link_content[0],
+                                              text_type=TextType.LINK,
+                                              url=link_content[1]))
+                    original_text = sections[1]
+                if original_text != "":
+                    new_nodes.append(TextNode(text=original_text,
+                                              text_type=TextType.RAW_TEXT))
+        return new_nodes
+                    
+                       
+                    
+
+                
+        return new_nodes
     
     def extract_markdown_images(text):
         return re.findall(InlineTextNode.REGX_IMAGE, text)
--- src/main.py
+++ src/main.py
@@ -2,13 +2,31 @@ from text_node import TextNode
 from parent_node import ParentNode
 from leaf_node import LeafNode
 from html_node import HTMLNode
+from inline_text_node import InlineTextNode
+from text_types import TextType
 import re
 
 
-text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
-regx= r"!\[(.*?)\]\((.*?)\)"
-test = re.findall(regx, "")
-print(test)
-regx_2 = r"\[(.*?)\]\((.*?)\)"
-text_2 = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
-test_2 = re.findall(regx_2, text_2)
+text = """This is text with an\
+![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)\
+and\
+![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png) AAAAND\
+[this is def link](https://google.com)"""
+test = TextNode(text=text, text_type=TextType.RAW_TEXT)
+
+
+
+
+
+
+
+
+
+
+for image in InlineTextNode.split_nodes_image([test]):
+    print(image)
+
+print("-------")
+
+for link in InlineTextNode.split_nodes_link([test]):
+    print(link)
