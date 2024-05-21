8b38b9fb604bc47ee9e6af872916a201c184f131
Crunc2h <frksythngl@hotmail.com>
2024-05-21 03:34:32 +0300

    Text node to html conversion added + Supported text types are also added

--- src/text_node.py
+++ src/text_node.py
@@ -1,10 +1,15 @@
+from text_types import TextType
+from leaf_node import LeafNode
+
 class TextNode:
     def __init__(self, 
                  text,
                  text_type,
                  url=None):
+        if text_type.lower() not in TextType.TYPES:
+            raise ValueError(f"Text type {text_type.lower()} is not supported.")
+        self.text_type = TextType.TYPES[TextType.TYPES.index(text_type.lower())]
         self.text = text
-        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
@@ -14,4 +19,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def to_html(self):
+        if self.text_type == TextType.RAW_TEXT:
+            return LeafNode(value=self.text).to_html()
+        elif self.text_type == TextType.BOLD:
+            return LeafNode(tag="b", value=self.text).to_html()
+        elif self.text_type == TextType.ITALIC:
+            return LeafNode(tag="i", value=self.text).to_html()
+        elif self.text_type == TextType.CODE:
+            return LeafNode(tag="code", value=self.text).to_html()
+        elif self.text_type == TextType.LINK:
+            return LeafNode(tag="a", value=self.text, props={"href": self.url}).to_html()
+        else:
+            return LeafNode(tag="img", value="", props={"src": self.url,
+                                                        "alt": self.text}).to_html()
     
---
+++ src/text_types.py
@@ -0,0 +1,13 @@
+class TextType:
+    RAW_TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+    TYPES = [RAW_TEXT,
+             BOLD,
+             ITALIC,
+             CODE,
+             LINK,
+             IMAGE]
